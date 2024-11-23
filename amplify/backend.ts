53ad1e4a310f4e05e71f6ee3f79145d4ac76419a
Abel Edwin <edwin1999997@gmail.com>
2024-11-23 00:07:30 -0600

    Conferences Departments Positions and Users chema

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { auth } from './auth/resource.js';
+import { data } from './data/resource.js';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
 });
-
-const { groups } = backend.auth.resources
-
-// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_iam.IRole.html
-groups["ADMINS"].role
